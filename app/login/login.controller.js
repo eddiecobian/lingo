be2a2cc2f08057f0477787ce50b6468fdfcefe36
eddiecobian <eddie.cobian19@gmail.com>
2017-03-21 10:14:22 -0700

    organizing code

--- app/login/login.controller.js
+++ app/login/login.controller.js
@@ -1,48 +1,45 @@
 (function () {
-     "use: strict"
+    "use: strict"
     angular
         .module('lingoApp')
         .controller('loginController', LoginController);
 
     LoginController.$inject = ['$scope', '$state', 'loginFactory', '$location'];
 
-    function LoginController($scope, $state, loginFactory, $location ) {
+    function LoginController($scope, $state, loginFactory, $location) {
 
         var vm = this;
 
         vm.login = login;
         vm.updateLogin = updateLogin;
         vm.validateUser = validateUser;
-        //vm.form = false;
 
-        ///// Post Login Input Values ////////////////////////////////
-         function login() {
+        ///// Post Login Input Values /////////////////
+        function login() {
 
-           vm.users= data.item;
-           console.log(data.item);
+            vm.users = data.item;
+            console.log(data.item);
+        }
 
-            //vm.data = data.data.items
-    
-}   
-        function updateLogin(){
+        function updateLogin() {
 
             var loginInfo = {
 
                 email: vm.email,
                 password: vm.password
             }
-          loginFactory.postUserLogin(loginInfo).then(validateUser);
+            loginFactory.postUserLogin(loginInfo).then(validateUser);
         }
-    
-    
-    function validateUser(data){
-        
-       if (data.data.isSuccessful == true) {
-              sessionStorage.setItem('user', data.data.item);
-                 $state.transitionTo('user-profile',{
-                     location:  false
-                 });
-             //window.location.replace = ('user-profile');
+
+
+        function validateUser(data) {
+
+            if (data.data.isSuccessful == true) {
+                sessionStorage.setItem('user', data.data.item);
+                $state.transitionTo('user-profile', {
+                    location: false
+                });
+                //window.location.replace = ('user-profile');
 
 
                 console.log(data);
